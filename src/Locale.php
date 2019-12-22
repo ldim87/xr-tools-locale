f8be1ceb7a009d1f1d25c1ba72d019c7242cace0
Admin <support@1000.menu>
2019-12-22 01:58:28 +0100

    Add files

---
+++ src/Locale.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author  Dmitriy Lukin <lukin.d87@gmail.com>
+ */
+
+
+namespace XrTools;
+
+/**
+ * :TODO: Localization utilities (time, language, geo)
+ */
+class Locale
+{
+
+	
+}
